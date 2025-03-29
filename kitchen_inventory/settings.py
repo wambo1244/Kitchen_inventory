3b9fed81d3dae125dbae6a2682a7f15611569fb2
brandonladen <brandonladen486@gmail.com>
2025-03-29 08:49:14 +0300

    resolved err

--- kitchen_inventory/settings.py
+++ kitchen_inventory/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/accounts/login/'
+LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/storekeeper/dashboard/'
